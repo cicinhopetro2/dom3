a2db9b31da7233ede3f95fc4848606d2408d4c95
Cicero Fernandes Gomes Leite <108589879+cicinhopetro2@users.noreply.github.com>
2023-07-13 11:52:45 -0300

    Add files via upload

---
+++ js/ex2_1a.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+    
+    function mostrarOla() {
+        
+        var nome = document.getElementById("nome").value;
+        
+        document.getElementById("resposta").textContent = "Olá " + nome;
+        }
+       
+        var mostrar = document.getElementById("mostrar");
+        
+        mostrar.addEventListener("click", mostrarOla);
